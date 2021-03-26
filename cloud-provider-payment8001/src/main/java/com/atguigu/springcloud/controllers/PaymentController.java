49744545ea7f462051f483be6baa80ee31766f23
fanhuaqiao <1802699850@qq.com>
2021-03-26 22:18:14 +0800

    20210326

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controllers/PaymentController.java
@@ -0,0 +1,43 @@
+package com.atguigu.springcloud.controllers;
+
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import com.atguigu.springcloud.services.PaymentService;
+import com.atguigu.springcloud.services.PaymentServiceImp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PaymentController {
+
+
+    @Autowired
+    private PaymentServiceImp paymentServiceImp;
+
+    @PostMapping("/payment/create")
+    public CommonResult createPayment(Payment payment){
+        int i = paymentServiceImp.create(payment);
+        if (i>0) {
+            return new CommonResult(200,"新增支付成功",i);
+        }else {
+            return new CommonResult(444,"失败了");
+        }
+    }
+
+    @GetMapping("/payment/get/{id}")
+    public CommonResult getPaymentById(@PathVariable Long id){
+        Payment paymentById = paymentServiceImp.getPaymentById(id);
+        if (paymentById!=null) {
+            return new CommonResult(200,"查询成功",paymentById);
+        }else {
+            return new CommonResult(404,"查询失败了");
+        }
+    }
+
+
+}
